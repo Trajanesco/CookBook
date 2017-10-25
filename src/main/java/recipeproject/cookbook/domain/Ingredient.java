3513af97b41cce274bc0f65aa983fa4daddc90ee
MatMic <trajanesco@gmail.com>
2017-10-25 18:15:57 +0200

    Make entities relations between Recipe and Notes(OneToOne), Recipe and Ingredients(OneToMany > ManyToOne)

--- src/main/java/recipeproject/cookbook/domain/Ingredient.java
+++ src/main/java/recipeproject/cookbook/domain/Ingredient.java
@@ -11,7 +11,8 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-//    private UniteOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
@@ -46,5 +47,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
