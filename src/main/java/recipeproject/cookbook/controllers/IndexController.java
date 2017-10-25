bec8aa9e08b2fe955fce1ce678451ec20c28eba7
MatMic <trajanesco@gmail.com>
2017-10-25 12:30:21 +0200

    Add starting files

---
+++ src/main/java/recipeproject/cookbook/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package recipeproject.cookbook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
